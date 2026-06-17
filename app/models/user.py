773b620d672cb4440c3974d623a52e624e93850d
Pearl Molefe <pdm.molefe@gmail.com>
2026-06-17 18:14:22 +0100

    add db session

--- app/models/user.py
+++ app/models/user.py
@@ -3,12 +3,19 @@ from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID  # Import PostgreSQL native UUID
 from sqlalchemy.orm import declarative_base
 
+# Database Connection 
+DATABASE_URL = "postgresql://admin_user:administrator@localhost:5432/misconception_db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
 
-    # 1. Primary key is now a UUID. 
+     # Explicitly configure SQLAlchemy to use the "auth" schema
+    __table_args__ = {"schema": "auth"}
+
+    # Primary key is a UUID. 
     # default=uuid.uuid4 tells SQLAlchemy to create a new unique ID for every new user.
     id = Column(
         UUID(as_uuid=True), 
@@ -22,3 +29,13 @@ class User(Base):
     password_hash = Column(String, nullable=False)
     last_login_at = Column(DateTime(timezone=True), nullable=True)
     created_at = Column(DateTime(timezone=True), nullable=True)
+
+    # Helper function to create tables automatically 
+    # Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
